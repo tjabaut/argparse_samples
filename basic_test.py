61b84411aa836780dfea0723dea92d634c495832
Tim Jabaut <tjabaut@gmail.com>
2022-08-09 18:06:32 -0400

    initial commit of smaples

---
+++ basic_test.py
@@ -0,0 +1,32 @@
+import argparse
+
+# # Create the Parser
+# parser = argparse.ArgumentParser()
+
+# # Add the Arguments
+# parser.add_argument('--foo')
+
+# # Execute the parse_args() method
+# args = parser.parse_args(['--foo', 'BAR'])
+
+# # Print the value(s) of the namespacep
+# print(vars(args))
+
+
+# create the top-level parser
+parser = argparse.ArgumentParser(prog='PROG')
+parser.add_argument('--foo', action='store_true', help='foo help')
+subparsers = parser.add_subparsers(help='sub-command help')
+
+# create the parser for the "a" command
+parser_a = subparsers.add_parser('a', help='a help')
+parser_a.add_argument('bar', type=int, help='bar help')
+
+# create the parser for the "b" command
+parser_b = subparsers.add_parser('b', help='b help')
+parser_b.add_argument('--baz', choices='XYZ', help='baz help')
+
+# parse some argument lists
+parser.parse_args(['a', '12'])
+
+parser.parse_args(['--foo', 'b', '--baz', 'Z'])
